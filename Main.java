b63be6386daceae665f4047cea65ad510aa24644
timothy busk mortensen <71074232+tbm-2303@users.noreply.github.com>
2020-10-08 21:15:19 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
-package task2;
+package task1;
+
+import java.sql.SQLOutput;
 
 public class Main {
-    Building building1 = new Building(10,2,3,true);
 
+    public static void main(String[] args) {
+        driver driver_1 = new driver("timmy", 23);
+        driver driver_2 = new driver("kent",51);
+        driver driver_3 = new driver( "rene", 29);
+
+        car car_1 = new car( "Ford", "focus", 2017, "station-car");
+        car_1.setDriver(driver_1);
+        System.out.println(car_1);
+        System.out.println(car_1.getDriver().toString());
+
+        car car_2 = new car("Opel", "Astra", 1999, "station-car");
+        car_2.setDriver(driver_1);
+        System.out.println(car_2 + ", "+ driver_1);
+    }
 }
+
+
